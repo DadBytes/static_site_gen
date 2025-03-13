d39125d9f4f631e27482043692e754c9288c306f
DadBytes <bpmckim@proton.me>
2025-03-13 14:23:51 -0400

    add child classes to htmlnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -13,8 +13,45 @@ class HTMLNode:
         for value in self.props.items():
             attribute = value[0]
             data = value[1]
-            attributes = attributes + f" {attribute}='{data}'"
+            attributes = attributes + f' {attribute}="{data}"'
         return attributes
 
     def __repr__(self):
         return f"tag={self.tag}, value={self.value}, children={self.children}, props={self.props}"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__()
+        self.tag = tag
+        self.value = value
+        self.props = props
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All leaf nodes must have a value.")
+        if self.tag is None:
+            return self.value
+        if self.props is None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__()
+        self.tag = tag
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("All parent nodes must have a tag.")
+        if self.children is None:
+            raise ValueError("All parent nodes must have children.")
+
+        child_output = ""
+        for child in self.children:
+            child_output += child.to_html()
+
+        return f"<{self.tag}>{child_output}</{self.tag}>"
