e7e7958fd90fee531531f4511a6704c3fe6a0187
DadBytes <bpmckim@proton.me>
2025-03-13 09:53:57 -0400

    add additional unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,19 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_miss_url(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node.url, None)
+
+    def test_url(self):
+        node = TextNode("This is a link node", TextType.LINK, "https://www.boot.dev/")
+        self.assertNotEqual(node.url, None)
+
+    def test_ineq(self):
+        node = TextNode("This is a normal node", TextType.NORMAL)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
 
 if __name__ == "__main__":
     unittest.main()
